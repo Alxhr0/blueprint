c192075903f4fa78196b51e69a6bb3fbb083ec67
Edward Groves <EdwardGroves2007@proton.me>
2026-08-09 19:31:43 -0400

    Core: init

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,26 +2,48 @@
 
 set -ouex pipefail
 
-# Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
-### Install packages
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux \
+  --init none \
+  --no-modify-profile
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+mkdir -p /nix
 
-# this installs a package from fedora repos
-dnf5 install -y tmux
+dnf5 install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release terra-gpg-keys
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr enable lionheartp/Hyprland 
+dnf5 -y config-manager setopt google-chrome.enabled=1
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-#### Example for enabling a System Unit File
+PACKAGES=(
+    hyprland
+    waybar
+    quickshell
+    google-chrome-stable
+    code
+    google-noto-fonts-common
+    google-noto-fonts-cjk
+    google-noto-fonts-emoji
+    google-noto-fonts
+    wl-kpbtr
+    steam
+    tmux
+    ghostty
+  )
+
+dnf -y install "${PACKAGES[@]}"
+
+dnf5 -y copr disable lionheartp/Hyprland 
+
+dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf config-manager setopt tailscale-stable.enabled=0
+dnf -y install --enablerepo='tailscale-stable' tailscale
 
 systemctl enable podman.socket
+systemctl enable tailscaled.service
+
+if command -v chcon > /dev/null; then
+    chcon -R -t unconfined_mgmt_t /nix || true
+fi
