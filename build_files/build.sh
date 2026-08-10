afc5084008be40425b568674e83be417a9c24b5c
Edward Groves <EdwardGroves2007@proton.me>
2026-08-09 22:22:28 -0400

    Get rid of terra and put a copr

--- build_files/build.sh
+++ build_files/build.sh
@@ -24,6 +24,7 @@ mkdir -p /nix
 
 dnf5 -y install fedora-workstation-repositories
 dnf5 -y copr enable lionheartp/Hyprland 
+dnf5 -y copr enable scottames/ghostty
 dnf5 -y config-manager setopt google-chrome.enabled=1
 
 PACKAGES=(
@@ -39,18 +40,53 @@ PACKAGES=(
 dnf5 -y install "${PACKAGES[@]}"
 
 dnf5 -y copr disable lionheartp/Hyprland 
+dnf5 -y copr disable scottames/ghostty
 
 mkdir -p /usr/etc/flatpak/system
 
 cat <<EOF >> /usr/etc/flatpak/system/install
-org.mozilla.firefox
-org.kde.okular
-org.gnome.Calculator
-com.valvesoftware.Steam
+io.github.tanaybhomia.Whisp
+com.discordapp.Discord
+com.heroicgameslauncher.hgl
+md.obsidian.Obsidian
+it.mijorus.gearlever
+com.vysp3r.ProtonPlus
+org.kde.krita
+org.inkscape.Inkscape
+com.github.johnfactotum.Foliate
+org.gnome.gitlab.ilhooq.Bookup
+com.super_productivity.SuperProductivity
+com.ticktick.TickTick
+org.prismlauncher.PrismLauncher
+com.protonvpn.www
+dev.vencord.Vesktop
+us.zoom.Zoom
+io.github.Faugus.faugus-launcher
+io.podman_desktop.PodmanDesktop
+com.jeffser.Alpaca
+net.cozic.joplin_desktop
+com.logseq.Logseq
+io.github.alainm23.planify
+com.vixalien.sticky
+org.zotero.Zotero
+com.adamcake.Bolt
+io.gitlab.news_flash.NewsFlash
+moe.launcher.the-honkers-railway-launcher
+moe.launcher.sleepy-launcher
+moe.launcher.an-anime-game-launcher
+info.febvre.Komikku
+com.rafaelmardojai.Blanket
+io.github.dvlv.boxbuddyrs
+page.kramo.Cartridges
+io.github.qwersyk.Newelle
+net.runelite.RuneLite
+io.github.faridjaff.StickyNotesCanvas
+org.localsend.localsend_app
+net.lutris.Lutris
 EOF
 
 cat <<EOF >> /usr/etc/flatpak/system/remove
-org.gnome.Tour
+org.mozilla.firefox
 EOF
 
 systemctl enable podman.socket
