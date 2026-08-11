13479f7d8edb33dbd13933509f5895de84605872
Edward Groves <EdwardGroves2007@proton.me>
2026-08-10 20:53:40 -0400

    forgot -y

--- build_files/build-edward.sh
+++ build_files/build-edward.sh
@@ -86,8 +86,8 @@ dnf5 -y copr enable scottames/ghostty
 dnf5 -y copr enable lionheartp/Hyprland
 dnf5 -y config-manager setopt google-chrome.enabled=1
 dnf5 -y config-manager setopt rpmfusion-nonfree-steam.enabled=1
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf5 -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 PACKAGES=(
     google-chrome-stable
