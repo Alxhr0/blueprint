7cf1f52729cbb03fcebe28aeb259e02f904c1d44
Edward Groves <EdwardGroves2007@proton.me>
2026-08-12 15:22:53 -0400

    Feat: detection of the right images

---
+++ build_files/build-holo.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+set -ouex pipefail
+
+cp -avf "/ctx/system_files.holo"/. /
+
+sed -i 's/^#Include = \/etc\/pacman.conf.d\/\*.conf/Include = \/etc\/pacman.conf.d\/\*.conf/' /etc/pacman.conf
+
+pacman -Syu --noconfirm
+
+PACKAGES=(
+    linux
+    linux-firmware
+
+    plasma-desktop
+    plasma-workspace
+    plasma-nm
+    plasma-pa
+    kdeplasma-addons
+    breeze
+    konsole
+    dolphin
+    firefox
+    kdeconnect
+    plasma-login-manager
+
+    steam
+    proton
+    gamemode
+    lib32-gamemode
+    mangohud
+    lib32-mangohud
+    wine-staging
+    giflib
+    lib32-giflib
+    libpng
+    lib32-libpng
+    libldap
+    lib32-libldap
+    libvulkan
+    lib32-libvulkan
+    libxcomposite
+    lib32-libxcomposite
+    libxinerama
+    lib32-libxinerama
+    libxrandr
+    lib32-libxrandr
+    opencl-driver
+    lib32-opencl-driver
+    lib32-gnutls
+    lib32-libpulse
+    libgudev
+    lib32-libgudev
+    alsa-lib
+    lib32-alsa-lib
+    alsa-utils
+
+    pipewire
+    pipewire-alsa
+    pipewire-audio
+    pipewire-ffado
+    pipewire-jack
+    pipewire-pulse
+    pipewire-zeroconf
+    wireplumber
+    sof-firmware
+    alsa-firmware
+    linux-firmware-intel
+)
+
+pacman -S --noconfirm --needed "${PACKAGES[@]}"
+
+pacman -Scc --noconfirm
+
+echo "uninitialized" > /etc/machine-id
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+
+systemctl enable plasmalogin
+
+systemctl --global enable pipewire.socket pipewire.service pipewire-pulse.service wireplumber.service
