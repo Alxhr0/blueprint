7f99e0b5458dbef442f04cd4f792a49ebffdff23
Edward Groves <EdwardGroves2007@proton.me>
2026-08-12 10:16:48 -0400

    Fix arch bootc

--- build_files/build-arch.sh
+++ build_files/build-arch.sh
@@ -5,14 +5,27 @@ cp -avf "/ctx/system_files.arch"/. /
 
 sed -i 's/^#Include = \/etc\/pacman.conf.d\/\*.conf/Include = \/etc\/pacman.conf.d\/\*.conf/' /etc/pacman.conf
 
+mkdir -p /var/cache/pacman/pkg
+
 pacman -Syu --noconfirm
 
 PACKAGES=(
     base
-    bootc
+    bubblewrap
+    dracut
     linux
     linux-firmware
     ostree
+    btrfs-progs
+    e2fsprogs
+    xfsprogs
+    dosfstools
+    skopeo
+    dbus
+    dbus-glib
+    glib2
+    shadow
+    openssh
     sudo
     systemd
     udev
