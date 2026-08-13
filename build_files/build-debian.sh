e8a86bc258da884a14bc423bc605b19df31ee57e
Edward Groves <EdwardGroves2007@proton.me>
2026-08-13 01:09:06 -0400

    fix: force symlinks in debian system stage for pre-existing paths

--- build_files/build-debian.sh
+++ build_files/build-debian.sh
@@ -63,13 +63,13 @@ rm -rf /{boot,home,root,srv,mnt,var,usr/local}
 
 mkdir -p /sysroot /boot /usr/lib/ostree /var
 
-ln -sT sysroot/ostree /ostree
-ln -sT var/roothome /root
-ln -sT var/srv /srv
-ln -sT var/mnt /mnt
-ln -sT var/opt /opt
-ln -sT var/home /home
-ln -sT var/usrlocal /usr/local
+ln -sfnT sysroot/ostree /ostree
+ln -sfnT var/roothome /root
+ln -sfnT var/srv /srv
+ln -sfnT var/mnt /mnt
+ln -sfnT var/opt /opt
+ln -sfnT var/home /home
+ln -sfnT var/usrlocal /usr/local
 
 printf 'd /var/home 0755 root root -\nd /var/srv 0755 root root -\nd /var/mnt 0755 root root -\nd /var/opt 0755 root root -\nd /var/usrlocal 0755 root root -\nd /var/roothome 0700 root root -\nd /run/media 0755 root root -\n' > /usr/lib/tmpfiles.d/bootc-base-dirs.conf
 
