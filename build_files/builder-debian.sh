ab6683ecd2c405263b821b9b163a9db4118bcb56
Edward Groves <EdwardGroves2007@proton.me>
2026-08-12 23:58:45 -0400

    fix: align debian builder with bootcrew/mono pattern

--- build_files/builder-debian.sh
+++ build_files/builder-debian.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -ouex pipefail
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs \
-    | sh -s -- -y --default-toolchain stable --profile minimal
-export PATH=/root/.cargo/bin:${PATH}
-
 git clone "https://github.com/bootc-dev/bootc.git" /tmp/bootc
 make -C /tmp/bootc bin install-all DESTDIR=/output
 rm -rf /tmp/bootc
