365639d3591a4f4269f4dc0e1a8c3e4dd7895a30
Edward Groves <EdwardGroves2007@proton.me>
2026-08-13 00:07:39 -0400

    fix: skip bind-mounted /var/cache and /var/log in aira cleanup

--- build_files/build-aira.sh
+++ build_files/build-aira.sh
@@ -74,7 +74,8 @@ ln -sfnT var/usrlocal /usr/local
 
 printf 'd /var/home 0755 root root -\nd /var/srv 0755 root root -\nd /var/mnt 0755 root root -\nd /var/opt 0755 root root -\nd /var/usrlocal 0755 root root -\nd /var/roothome 0700 root root -\nd /run/media 0755 root root -\n' > /usr/lib/tmpfiles.d/bootc-base-dirs.conf
 
-rm -rf /{boot,home,root,srv,mnt,var,usr/local}
+rm -rf /var/cache/* /var/log/*
+rm -rf /{boot,home,root,srv,mnt,usr/local}
 
 rm -rf /tmp/* /run/*
 
