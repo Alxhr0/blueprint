7b135a123fc29e4d21c4cfa1ebe22b5f0ff7d190
Edward Groves <EdwardGroves2007@proton.me>
2026-08-14 15:57:54 -0400

    Accept linux-fw-redistributable license and ~amd64 keywords in Gentoo build

--- build_files/base/build-gentoo.sh
+++ build_files/base/build-gentoo.sh
@@ -5,6 +5,9 @@ cp -avf "/ctx/system_files"/. /
 cp -avf "/ctx/system_files/global"/. /
 cp -avf "/ctx/system_files/gentoo"/. /
 
+echo 'ACCEPT_LICENSE="-* @BINARY-REDISTRIBUTABLE linux-fw-redistributable"' >> /etc/portage/make.conf
+echo 'ACCEPT_KEYWORDS="~amd64"' >> /etc/portage/make.conf
+
 emerge --sync --quiet
 
 PACKAGES=(
