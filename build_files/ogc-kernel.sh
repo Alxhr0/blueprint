179d477586d2b93de6199857c0e0cf12f7d2e963
Edward Groves <EdwardGroves2007@proton.me>
2026-08-14 05:05:49 -0400

    Fix(ogc-kernel): temporarily disable pacman network sandbox for dracut

--- build_files/ogc-kernel.sh
+++ build_files/ogc-kernel.sh
@@ -36,4 +36,6 @@ KERNEL_VERSION="$(basename "$(find /usr/lib/modules -maxdepth 1 -type d | grep -
 DRACUT_NO_XATTR=1 dracut --force --no-hostonly --reproducible --zstd --verbose \
     --kver "$KERNEL_VERSION" "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
 
+sed -i '/^DisableSandboxNetwork$/d' /etc/pacman.conf
+
 rm -rf "$OGCI_DIR"
