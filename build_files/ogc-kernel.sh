f11b5ed893b1ab12f4bbc8063f9e55c1b55419ef
Edward Groves <EdwardGroves2007@proton.me>
2026-08-14 05:05:15 -0400

    Fix(ogc-kernel): remove stock linux pkg, allow dracut network, rebuild initramfs

--- build_files/ogc-kernel.sh
+++ build_files/ogc-kernel.sh
@@ -24,4 +24,16 @@ done
 
 pacman -U --noconfirm "${OGCI_DIR}"/*.pkg.tar.zst
 
+if pacman -Q linux >/dev/null 2>&1; then
+    pacman -Rns --noconfirm linux
+fi
+
+if ! grep -q '^DisableSandboxNetwork' /etc/pacman.conf; then
+    sed -i '/^\[options\]/a DisableSandboxNetwork' /etc/pacman.conf
+fi
+
+KERNEL_VERSION="$(basename "$(find /usr/lib/modules -maxdepth 1 -type d | grep -v -E '\.img$' | tail -n 1)")"
+DRACUT_NO_XATTR=1 dracut --force --no-hostonly --reproducible --zstd --verbose \
+    --kver "$KERNEL_VERSION" "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
+
 rm -rf "$OGCI_DIR"
