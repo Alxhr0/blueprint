28fc5f4438ff44496540c0b97e45edf90775fd8a
Edward Groves <EdwardGroves2007@proton.me>
2026-08-16 13:14:17 -0400

    Fix both aira and edward for nix

--- build_files/aira/build-aira.sh
+++ build_files/aira/build-aira.sh
@@ -19,8 +19,6 @@ echo '. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' \
   > /etc/profile.d/nix.sh
 PATH="/nix/var/nix/profiles/default/bin:${PATH}"
 
-systemctl enable nix-daemon.socket nix-daemon.service
-
 mkdir -p /etc/nix && \
 echo "experimental-features = nix-command flakes" >> /etc/nix/nix.conf
 
--- build_files/edward/build.sh
+++ build_files/edward/build.sh
@@ -5,6 +5,15 @@ cp -avf "/ctx/system_files/global"/. /
 cp -avf "/ctx/system_files/edward"/. /
 cp -avf "/ctx/system_files/arch"/. /
 
+curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install | sh -s -- --daemon
+
+echo '. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' \
+  > /etc/profile.d/nix.sh
+PATH="/nix/var/nix/profiles/default/bin:${PATH}"
+
+mkdir -p /etc/nix && \
+echo "experimental-features = nix-command flakes" >> /etc/nix/nix.conf
+
 pacman -Syu --noconfirm
 
 PACKAGES=(
