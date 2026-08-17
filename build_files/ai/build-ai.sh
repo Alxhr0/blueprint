9596f84e2d737ca25e350626e9d3a739ac201bda
Edward Groves <EdwardGroves2007@proton.me>
2026-08-16 20:45:46 -0400

    Feat: more ai packages

--- build_files/ai/build-ai.sh
+++ build_files/ai/build-ai.sh
@@ -3,12 +3,23 @@ set -ouex pipefail
 
 cp -avf "/ctx/system_files/global"/. /
 
+curl -fsSL https://bun.sh/install | bash
+curl -fsSL https://pi.dev/install.sh | sh
+bun add -g opencode-ai
+bun add -g @kilocode/cli
+curl -LsSf https://llama.app/install.sh | sh
+curl -fsSL https://ollama.com/install.sh | sh
+curl -fsSL https://cli.kiro.dev/install | bash
+curl -fsSL https://antigravity.google/cli/install.sh | bash
+curl https://cursor.com/install -fsS | bash
+
 PACKAGES=(
     build-essential
     git
     curl
     wget
     vim
+    tmux
     nano
     python3
     python3-pip
