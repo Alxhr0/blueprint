bebd2ee32ceaf45b05f7b7d0263b4d5df088547c
Edward Groves <EdwardGroves2007@proton.me>
2026-08-17 21:03:26 -0400

    added docker group service

--- build_files/edward/40-services.sh
+++ build_files/edward/40-services.sh
@@ -6,10 +6,15 @@ systemctl enable gdm.service
 systemctl enable firewalld.service
 systemctl enable fwupd.service
 systemctl --global enable podman-auto-update.timer
+systemctl enable tailscaled.service
+systemctl enable brew-setup.service
 systemctl enable brew-bundle.service
+systemctl enable brew-update.timer
+systemctl enable brew-upgrade.timer
 systemctl enable flatpak-preinstall.service
 systemctl enable flatpak-appstream-refresh.service
 systemctl enable ublue-system-setup.service
 systemctl --global enable ublue-user-setup.service
 systemctl enable docker.service
 systemctl enable containerd.service
+systemctl enable add-user-to-docker.service
