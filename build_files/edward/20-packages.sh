562af14bb212aadb991b5d78847337aaa2722d2a
Edward Groves <EdwardGroves2007@proton.me>
2026-08-17 21:11:56 -0400

    Changed where docker curl happens

--- build_files/edward/20-packages.sh
+++ build_files/edward/20-packages.sh
@@ -7,4 +7,7 @@ PKGS_TOML="/run/context/build_scripts/packages/base.toml"
 # Install packages from manifest
 readarray -t INSTALL_PKGS < <($READ_PKGS "$PKGS_TOML" install)
 
+# Docker CE repo (must come before docker package install)
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
 dnf -y install "${INSTALL_PKGS[@]}"
