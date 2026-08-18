b8866151cb5c130ccdcb8c47204dc542c9fb9a43
Edward Groves <EdwardGroves2007@proton.me>
2026-08-18 16:25:30 -0400

    Added a remove docker stuff for the new docker stuff I add

--- build_files/edward/overrides/layer/20-docker.sh
+++ build_files/edward/overrides/layer/20-docker.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+# Remove any pre-installed distro containerd / docker packages to avoid obsoletion/replacement conflicts
+dnf -y remove \
+    containerd \
+    docker \
+    docker-client \
+    docker-client-latest \
+    docker-common \
+    docker-latest \
+    docker-latest-logrotate \
+    docker-logrotate \
+    docker-engine \
+    moby-engine || true
+
 # Docker CE (newer than the LTS default container stack)
 curl -fsSL https://download.docker.com/linux/centos/docker-ce.repo \
     -o /etc/yum.repos.d/docker-ce.repo
